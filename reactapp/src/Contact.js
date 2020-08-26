ae783e953bc9fbb6621052a07700d39980e8568a
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-26 22:25:28 +0530

    make a diffrent component

---
+++ reactapp/src/Contact.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+
+function Contact() {
+    
+        return(
+            <div className="contact-page">
+            <div>
+                <h1>This is contact page</h1>
+            </div>
+
+            </div>
+        )
+    
+}
+export default Contact;
