d625efdb0aa143a19b7142d7f9a83f5411d4b01d
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-26 22:25:47 +0530

    add a child component

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Contact from './Contact'
 function App() {
   return (
     <div className="App">
-      <h1>
-        first react app
-      </h1>
+      <h1> first react app</h1>
+      <Contact />
     </div>
   );
 }
