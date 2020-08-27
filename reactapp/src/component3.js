281d1d78481371352913c46cab6b47b63b7978c0
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-27 10:59:31 +0530

    add a function based component

---
+++ reactapp/src/component3.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Component3 = (props) =>{
+    return(
+        <div className= "Component3">
+            <h1>thi is a function based component</h1>
+
+        </div>
+    )
+}
+export default Component3
