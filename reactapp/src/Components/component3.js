f8ed81be865f6ba7ca0df49a615c9e65bf1130c6
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-27 21:31:25 +0530

    call the delete method

--- reactapp/src/Components/component3.js
+++ reactapp/src/Components/component3.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // A func based component with {list} passed manually as a props using destructuring
-const Component3 = ({list}) =>{
+const Component3 = ({list,deleteFromList}) =>{
 
     //this  "map" method itereats list one by one storing in "item" and then store in "mylist" 
     const mylist = list.map(item =>{
@@ -12,7 +12,8 @@ const Component3 = ({list}) =>{
 
                 <h2> name: {item.name}</h2>
                 <h2> price: {item.price}</h2>
-                
+                <button onClick={()=>{deleteFromList(item.id)} } >Delete </button>
+                 
             </div>
         )
     })
@@ -20,7 +21,7 @@ const Component3 = ({list}) =>{
         <div className= "Component3">
             
             <h1>this is a function based component</h1>
-            <h2>a list  of 2 elements is iterated below </h2>
+            <h2>a list  of {mylist.length} elements is iterated below </h2>
             
             {/* linking the iterated list to the main return method */}
             {mylist}
