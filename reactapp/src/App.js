f63084cf9496d8c34baa1a06c014452b7c457651
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-27 11:02:32 +0530

    attach the fun based comp

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Contact from './Contact'
+import Component3 from './component3'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="head"> first react app (this is a class component)</h1>
         {/* <button onClick={this.showcontact} > show contact app</button > */}
+
+        {/* //this is how you nest a  class based component */}
+
         <Contact/>
+      {/* this is how you nest a function based compopnent */}
+        { Component3() }
       </div>
     )
   };
