546f3fff9b6806a08900f0628b4805118d9833c4
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-27 10:34:56 +0530

    add border styling

--- reactapp/src/Contact.js
+++ reactapp/src/Contact.js
@@ -4,7 +4,9 @@ class Contact extends Component {
     render(){
         return(
             <div className="contact-page">
+
             <div>
+                <h1>this is another class based component</h1>
                 <h1>This is contact page</h1>
             </div>
 
